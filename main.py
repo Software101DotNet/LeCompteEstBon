a6182b0868afd74cde2eb6aadd22cfe15badcf7e
Anthony (MBA-M4) <software101.net@gmail.com>
2025-12-14 17:26:17 +1100

    Add initial implementation of game solution algorithms and expressions

--- main.py
+++ main.py
@@ -1,23 +1,115 @@
-# Create a list of all the possibilities of six numbers from 
-# the set of 14 numbers consisting of 100,75,50,25,10,9,8,7,6,5,4,3,2,1
-# There are 3003 combinations of choosing 6 numbers from a set of 14 numbers.
-def SetOfNumbers():
-    numbers = [100, 75, 50, 25, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
-    from itertools import combinations
-    return list(combinations(numbers, 6))
+# Copyright (C) 2025 Anthony Ransley
+# https://github.com/Software101DotNet
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-def CountNumberSet():
-    return len(SetOfNumbers())
 
-# given a set of six numbers, return a set of all permutations of those numbers
-def PermutationsOfSet(number_set):
+def SetOfAllSixNumberCombinations():
+	from itertools import combinations
+	#numbers = [100, 75, 50, 25, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+	numbers = [100, 75, 50, 25, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]  # Game drawn from 2 sets of single digit numbers
+	return list(combinations(numbers, 6))
+
+
+# for a given game of 6 numbers, return the complete set of all the possible sequences that can be made from the 6 number numbers
+def GameSolutionsPermutations(gameSet):
     from itertools import permutations
-    return list(permutations(number_set))
 
-# 
+    allPermutations=list()
+    
+    # add all the unique gameSet numbers to the allPermutations solutions set
+    for i in range(1,6):
+        for number in permutations(gameSet, i):
+            if number not in allPermutations:
+                allPermutations.append(number)
+
+    return allPermutations
+
+     
+from itertools import permutations, combinations
+
+def PossibleGameSolutions(numbers):
+    ops = ['+', '-', '*', '/']
+    results = set()
+
+    def build_expr(nums, exprs):
+        if len(nums) == 1:
+            results.add(exprs[0])
+            return
+
+        for i in range(len(nums)):
+            for j in range(len(nums)):
+                if i == j:
+                    continue
+
+                a, b = nums[i], nums[j]
+                ea, eb = exprs[i], exprs[j]
+
+                remaining_nums = [
+                    nums[k] for k in range(len(nums)) if k not in (i, j)
+                ]
+                remaining_exprs = [
+                    exprs[k] for k in range(len(exprs)) if k not in (i, j)
+                ]
+
+                for op in ops:
+                    if op == '/' and b == 0:
+                        continue
+
+                    try:
+                        if op == '+':
+                            val = a + b
+                        elif op == '-':
+                            val = a - b
+                        elif op == '*':
+                            val = a * b
+                        elif op == '/':
+                            val = a / b
+                    except ZeroDivisionError:
+                        continue
+
+                    new_expr = f"({ea}{op}{eb})"
+                    build_expr(
+                        remaining_nums + [val],
+                        remaining_exprs + [new_expr]
+                    )
+
+    # Use all subsets of size >= 2
+    for r in range(2, len(numbers) + 1):
+        for subset in combinations(numbers, r):
+            for perm in permutations(subset):
+                build_expr(list(perm), [str(x) for x in perm])
+
+    return sorted(results)
+
+
 def main():
-    for combo in SetOfNumbers():
-        print(combo)
+
+    gameSet = SetOfAllSixNumberCombinations()
+    print(f"Number of possible game combinations: {len(gameSet):,}")
+
+    GameNumberSet = GameSolutionsPermutations(gameSet[0])
+    print(f"Number of possible solutions for game combination {len(GameNumberSet):,}")
+
+    possibleSolutions = PossibleGameSolutions(GameNumberSet)
+    print(f"Number of possible soutions for game {len(possibleSolutions):,}")
+
+    print ("\nList of possible solutions:")
+    solution =  PossibleGameSolutions([100, 25, 8, 5, 2])
+for index, value in enumerate(sorted(solution)):
+    print(f"{index:5d}  {value}")
+
 
 
 if __name__ == "__main__":
